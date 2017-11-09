efd9165ce29880db175f8f252cbfcc85b1b66933
Aynolor <hi@aynolor.com>
2017-11-09 10:57:48 +0100

    Rendre paramétrable le retour haptique au refresh

---
+++ SuperHFRplus/Helpers/UIImpactFeedbackGenerator+UserDefaults.h
@@ -0,0 +1,14 @@
+//
+//  UIImpactFeedbackGenerator+UserDefaults.h
+//  SuperHFRplus
+//
+//  Created by Aynolor on 09.11.17.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImpactFeedbackGenerator (UserDefaults)
+
+- (void)impactOccurredWithDefaults;
+
+@end
